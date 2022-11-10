648a2b524229ca42ebd4475cabdeb852a749275e
JYOTHSNA1331 <60293594+JYOTHSNA1331@users.noreply.github.com>
2022-11-10 21:11:35 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express();
+const data1 = require('./Routes/bollywood')
+
+app.use('/home',data1)
+app.listen(process.env.PORT||8080,() => {
+    console.log('server running')
+})
